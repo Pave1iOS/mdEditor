f6a3e23e474675f152361abf9281aac46cc0aa70
Pave1iOS <149584590+Pave1iOS@users.noreply.github.com>
2024-06-06 00:29:04 +0300

    Delete .github/build.sh

--- .github/build.sh
+++
@@ -1,9 +0,0 @@
-curl -Ls https://install.tuist.io | bash
-tuist install 3.42.2
-tuist fetch
-tuist generate
-xcodebuild clean -quiet
-xcodebuild build-for-testing\
-    -workspace 'mdEditor.xcworkspace' \
-    -scheme 'mdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro' 
