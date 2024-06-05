a52062cb124da7158b23e0b8dcdf617905f9feaf
Pave1iOS <149584590+Pave1iOS@users.noreply.github.com>
2024-06-05 23:44:29 +0300

    Add files via upload

---
+++ mdEditor/Sources/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mdEditor
+//
+//  Created by Pavel Gribachev on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		
+		view.backgroundColor = .blue
+	}
+
+
+}
+
