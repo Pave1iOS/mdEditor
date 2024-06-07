1b767514d1a1a5dac383bc7fb784f2afd60ff50f
Pave1iOS <PGribachev94@yandex.ru>
2024-06-07 19:30:09 +0300

    reed project.swift

--- Project.swift
+++ Project.swift
@@ -1,15 +1,19 @@
 import ProjectDescription
 
-public var scripts: [TargetScript] {
-	
-	var scripts = [TargetScript]()
-	
-	let swiftLintScriptString = "SwiftLint/swiftlint --fix && SwiftLint/swiftlint"
-	let swiftLintScript = TargetScript.post(script: swiftLintScriptString, name: "SwiftLint")
-	
-	scripts.append(swiftLintScript)
-	return scripts
-}
+let infoPlist: [String: Plist.Value] = [
+	"UIApplicationSceneManifest": [
+		"UIApplicationSupportsMultipleScenes": false,
+		"UISceneConfigurations": [
+			"UIWindowSceneSessionRoleApplication": [
+				[
+					"UISceneConfigurationName": "Default Configuration",
+					"UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+				]
+			]
+		]
+	],
+	"UILaunchStoryboardName": "LaunchScreen"
+]
 
 let project = Project(
 	name: "mdEditor",
@@ -19,27 +23,14 @@ let project = Project(
 			name: "mdEditor",
 			platform: .iOS,
 			product: .app,
-			bundleId: "mdEditor" ,
-			infoPlist: "Resources/Info.plist",
+			bundleId: "com.ios.mdEditor" ,
+			infoPlist: .extendingDefault(with: infoPlist),
 			sources: ["Sources/**"],
 			resources: ["Resources/**"],
-			scripts: scripts,
 			dependencies: [
 				/* Target dependencies can be defined here */
 				/* framework(path: "framework") */
 			]
 		),
-		
-		Target (
-			name: "mdEditorTests",
-			platform: .iOS,
-			product: .unitTests,
-			bundleId: "mdEditorTests",
-			infoPlist: "Resources/Info.plist",
-			sources: ["Tests/**"],
-			dependencies: [
-				.target(name: "mdEditor")
-			]
-		)
 	]
 )
