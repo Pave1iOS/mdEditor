c5df1a25affcade9d416774e7da34575206d1db2
Pavel <149584590+Pave1iOS@users.noreply.github.com>
2024-06-08 03:15:36 +0300

    Delete Targets directory

--- Targets/MdEditorKit/Sources/MdEditorKit.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public final class MdEditorKit {
-    public static func hello() {
-        print("Hello, from your Kit framework")
-    }
-}
--- Targets/MdEditorUI/Tests/MdEditorUITests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class MdEditorUITests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("MdEditorUI", "MdEditorUI")
-    }
-}
