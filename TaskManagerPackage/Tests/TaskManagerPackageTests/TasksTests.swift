fc0f25fb4bf942219dcaf666353bae8e2a302b0b
Pave1iOS <PGribachev94@yandex.ru>
2024-06-14 17:05:56 +0300

    task test almost finished

--- TaskManagerPackage/Tests/TaskManagerPackageTests/TasksTests.swift
+++ TaskManagerPackage/Tests/TaskManagerPackageTests/TasksTests.swift
@@ -21,5 +21,19 @@ final class TasksTests: XCTestCase {
 		XCTAssertTrue(sut.completed)
 	}
 	
+	func test_regularTask_setting() {
+		let regularTask = RegularTask(title: "RegularTask", completed: true)
+				
+		XCTAssertTrue(!regularTask.title.isEmpty)
+		XCTAssertTrue(regularTask.completed)
+	}
 	
+	func test_importantTask_setting() {
+		let importantTask = ImportantTask(title: "ImportantTask", taskPriority: .high, createDate: .now)
+		importantTask.completed = true
+		
+		XCTAssertTrue(!importantTask.title.isEmpty)
+		XCTAssertTrue(importantTask.taskPriority.rawValue >= 0)
+		XCTAssertTrue(importantTask.completed)
+	}
 }
