c885c94ddfeb3382bed47bb85a2b474a58f7d318
Pave1iOS <PGribachev94@yandex.ru>
2024-07-04 17:20:32 +0300

    fix deadline

--- Project/Sources/Scenes/TodoListScene/TodoListPresenter.swift
+++ Project/Sources/Scenes/TodoListScene/TodoListPresenter.swift
@@ -47,7 +47,7 @@ final class TodoListPresenter: ITodoListPresenter {
 			let result = TodoListModel.ViewModel.ImportantTask(
 				title: task.title,
 				completed: task.completed,
-				deadLine: "Deadline: \(task.deadLine)",
+				deadLine: "\(L10n.deadline): \(task.deadLine)",
 				priority: "\(task.taskPriority)"
 			)
 			return .importantTask(result)
